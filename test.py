41167b8e5edd2de67c6a1e39850f4bfcf2b77ce3
Caduuuuu <95059085+Caduuuuu@users.noreply.github.com>
2022-10-09 22:38:48 -0400

    turned test.py into a little game

--- test.py
+++ test.py
@@ -1,8 +1,20 @@
+import random
 
-income = input("what is your income number?")
-if income == 4:
-    print('the income is corect')
-else: 
-    offset = int(income) - 4
-    print("income is off by: ", offset) 
+intro = "abv"
+while intro != "Y":
+    intro = input("Welcome to my little test game! you will have 10 tries to guess what number I am thinking. Type Y and press enter to continue:    ")
 
+loopcount = 0
+while loopcount < 9:
+    answer = input("what number between 1 and 10 am I guessing right now?   ")
+    x = random.randint(0,10)
+    #print('-----------------------------------')
+    if answer == x:
+        print('Well done, the number is corect!')
+    else: 
+        offset = int(answer) - x
+        print("The number your guessed is off by: ", offset) 
+    print('-----------------------------------')
+    loopcount += 1
+
+quit()
